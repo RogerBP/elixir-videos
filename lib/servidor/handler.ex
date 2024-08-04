7b34ee166c36f884762ab2f077c109812348bac5
Roger B. Pessoa <rogerbpessoa@gmail.com>
2024-08-04 10:52:12 -0300

    Import

--- lib/servidor/handler.ex
+++ lib/servidor/handler.ex
@@ -1,9 +1,12 @@
 defmodule Servidor.Handler do
+  # import Servidor.Plugins
+  import Servidor.Plugins, only: [parse: 1, rewrite: 1, route: 1, format_response: 1]
+
   def handle(request) do
     request
-    |> Servidor.Plugins.parse()
-    |> Servidor.Plugins.rewrite()
-    |> Servidor.Plugins.route()
-    |> Servidor.Plugins.format_response()
+    |> parse()
+    |> rewrite()
+    |> route()
+    |> format_response()
   end
 end
