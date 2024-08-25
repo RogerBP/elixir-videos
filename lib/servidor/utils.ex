f77a634682f62c93bbeb73262457cf7ca8c19fcd
Roger B. Pessoa <rogerbpessoa@gmail.com>
2024-08-25 11:58:47 -0300

    List to map com reduce / Headers

--- lib/servidor/utils.ex
+++ lib/servidor/utils.ex
@@ -1,10 +1,18 @@
 defmodule Servidor.Utils do
-  def list_to_map([head | tail], map) do
-    head_list = String.split(head, ": ")
-    [key, value] = head_list
-    new_map = Map.put(map, key, value)
-    list_to_map(tail, new_map)
-  end
+  # def list_to_map([head | tail], map) do
+  #   head_list = String.split(head, ": ")
+  #   [key, value] = head_list
+  #   new_map = Map.put(map, key, value)
+  #   list_to_map(tail, new_map)
+  # end
 
-  def list_to_map([], map), do: map
+  # def list_to_map([], map), do: map
+
+  def list_to_map(list, map) do
+    Enum.reduce(list, map, fn head, map ->
+      head_list = String.split(head, ": ")
+      [key, value] = head_list
+      Map.put(map, key, value)
+    end)
+  end
 end
